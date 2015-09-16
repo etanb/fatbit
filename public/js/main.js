669b1920b3632222d042bff46d49a1835737df23
Etan <eberkowitz@salesforce.com>
2015-09-15 17:09:50 -0700

    beta of fatbit done

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,24 @@
 $(function() {
-    // For silly homepage animation
-    $(".cloud").draggable();
+	if( $(location).attr('pathname') === "/" ){
+		// For silly homepage animation
+		$(".cloud").draggable();
 
-    $(".login-container").click( function() {
-    	window.location.href = '/authorize';
-    	return false;
-    })
+		$(".login-container").click( function() {
+			window.location.href = '/authorize';
+			return false;
+		})
+	} else if ( $(location).attr('pathname') === "/greetings" ) {
+		// For silly card flipping animation
+		$('.card').click(function(){
+		  $(this).toggleClass('flipped');
+		});
+		// For random healthy image for card
+		var healthyArray = ["/public/img/apple.svg", "/public/img/banana.svg", "/public/img/carrot.svg"],
+			fattyArray = ["/public/img/donut.svg", "/public/img/cupcake.svg", "/public/img/burger.svg"],
+			healthImg = healthyArray[Math.floor(Math.random() * healthyArray.length)],
+			fatImg = fattyArray[Math.floor(Math.random() * fattyArray.length)];
+		$("#healthyURL").attr('src', healthImg);
+		$("#fatURL").attr('src', fatImg);
+	}
  });
+
